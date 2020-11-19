e65c115a6021e514cd579cce5e45f47ecffca915
alnarolekoala <73858074+alnarolekoala@users.noreply.github.com>
2020-11-20 00:43:45 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,210 +1,51 @@
-/*alert('calcul');
-do
-var n = parseInt(prompt('Saisir un nombre'));
-while(isNaN(n)) 
+/*var a = window.prompt("écrire un nombre");
 
 
-do
-var c = parseInt(prompt('Saisir un deuxieme nombre'));
-while(isNaN(c))
-
- 
-do
-var s = prompt('saisir un signe parmis : +, -, *, /')
-while(!(s != '+' || s != '-' || s != '*' || s != '/'))*/
-
-
-
-/*var s = prompt('Saisir un signe parmis +, -, *, /');
-if(c != 0)
+if(a%2 == 0)
 {
-switch (s)
-{   
-  case "+" :
-     alert("choix +");  
-     break; 
-
-  case "-" : 
-  alert("choix -"); 
-  break;   
-
-  case "*" :
-    alert("choix *");  
-    
-     break;  
-
-  case "/" :
-    alert("choix /");  
-
-     break;
-
-  default: 
-     alert("ne figure pas ...");
+    document.write("nombre pair")
+}
+else
+{
+    document.write("nompre impair");
 } 
+
+var c
+var b = window.prompt("quelle est votre année de naissance?");
+parseInt(b);
+c = 2020 - b;
+window.alert('vous avez :' +c+'ans');
+if(c >=18){
+    window.alert('vous êtes majeur')
 }
-
-else if(c = 0)
-{
-    switch (s)
-    {   
-      case "+" :
-         alert("choix +");  
-         break; 
-    
-      case "-" : 
-      alert("choix -"); 
-      break;   
-    
-      case "*" :
-        alert("choix *");  
-        
-         break;  
-    
-      default: 
-         alert("ne figure pas ...");
-    }  
-}
-
-
- if(s == "+") {
-     window.alert(n + c);
- }
-
- else if(s == '-') {
-     window.alert(n - c);
- }
-
- else if(s == '*') {
-     window.alert(n * c);
-}
-
- else if(s == '/'&& c != 0) {
-     window.alert(n / c);
-}
- else {
-     window.alert('error div 0 impossible');
+else {
+    window.alert('vous êtes mineur')
 }*/
-
-/*var n = 0;
-var saisi;
-
-do {
-  n++;
-  saisi =prompt('Saisissez le prénom' +n);
-console.log(n);
-console.log(saisi);
-
-}  
-while(saisi!= "");
-
-var N = 0
-
-while (true) {
-  console.log(N);
-
-  if(n == N) {
-      break;
-    }
-    
-    N++
-}*/
+var r
+var n = parseInt(prompt('Saisir un nombre'));
+var s = prompt('Saisir un signe parmis (+, -, *, /) ');
+var c = parseInt(prompt('Saisir un deuxieme nombre'));
 
 
-
-/*var numb;
-var somme = 0;
-var all = Array();
-var entier = 15;
-do {
-  numb = entier--;
-    //document.write(' nombre ' , numb, ' ');
-    //emp++;
-    all.push(numb);
-    console.log(all);
+if(s == "+") {
+    window.alert(n + c);
 }
-while (numb != 0)
 
-var taille = 0
-var somme;
-
-do {
-  somme = somme + all[taille];
-  taille++; 
+else if(s == '-') {
+    window.alert(n - c);
 }
-while(taille < 16 )
 
-document.write('Somme = ' ,somme);
-document.write(' moyenne : ' ,somme/16);*/
-
-/*var N = 5;
-var X = 7;
-var mul = 1
-
-
-do { 
-  document.write('7x',mul ,'=' ,X * mul,' ');
-  mul++;
+else if(s == '*') {
+    window.alert(n * c);
 }
-while(N >= mul)*/
 
-
-/*
-var lettre = prompt('écrire un mot')
-var tableau = [];
-var y;
-var nb = lettre.length;
-//alert(nb);
-var i = 0;
-for(i=0; i<nb; i++)
-{
-  var one = lettre.substr(i,1);
-  
-  //alert(one);
-  
-  if(y = one.match(/[aeiou]/g));
-   {
-     if (y != null) {
-      console.log('-1');
-      tableau.push(y);
-     }
-  }
-  
-  console.log(tableau);
-
- }
- document.write('nombre de voyelle : ' ,tableau.length); */
-
-var chemin=prompt("chemin ");
+else if(s == '/'&& c != 0) {
+    window.alert(n / c);
+}
+else {
+    window.alert('error');
+}
 
 
 
 
- function maFonction(x) 
-    {
-
-      var ton_chemin=x;
-
-      alert("voici ton chemin : " +ton_chemin );
-
-     var plop1 = 123;       
-       return plop1;
-    }
-
-    function maFonction2() 
-    {
-       plop2 = 456;     
-    }
-
-    maFonction(chemin);
-
-
-
-
-
-    plop1 = maFonction();
-
-    console.log("fonction 1 / plop1 : "+plop1);
-
-    maFonction2();
-
-    console.log("mafonction2 > plop2 : "+plop2);
